dada3bb943a321eeefd0195aaeb1bf2438387916
RoBGlaBe - Hydra <rgladebeucke+github@gmail.com>
2020-07-31 18:13:18 +0200

    Added 3 more tiny-analysis functions

--- events_tiny.py
+++ events_tiny.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import analysis_tools as at
 import tiny_analysis as ta
 
@@ -39,7 +40,7 @@ def event_waveform(events,
     s2_color   -- Plot color of main S2 (default 'g')
     max_time   -- Time in ns relative to first peaks start time
                   after which peaks are not plotted (int)
-    is_log     -- Wheather or not to plot y-axis logarithmicly (default False)
+    is_log     -- Whether or not to plot y-axis logarithmicly (default False)
     figsize    -- mpl figsize (tuple)
     kwargs     -- Any kwargs plt.plot accepts, except color.
     """
@@ -64,6 +65,7 @@ def event_waveform(events,
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    plt.tight_layout()
 
     if is_log:
         plt.ylim(0.1, None)
@@ -71,4 +73,74 @@ def event_waveform(events,
     else:
         plt.axhline(0, c='k', alpha=0.2)
 
+
+
+@ta.tiny_analysis(requires=('event_basics', 'event_krypton_basics'))
+def area_width(
+            events,
+            x='s2_area',
+            y='s2_range_50p_area',
+            cuts=None,
+            xbins=None,
+            ybins=None,
+            xlabel=None,
+            ylabel=None,
+            title='',
+            cmap='plasma',
+            xlog=True,
+            ylog=True,
+            figsize=(9, 5),
+            **kwargs):
+
+    """Show 2D histogram of events versus s2_area(x) and s2_range_50p_area(y) (default).
+
+    Arguments for decorator:
+    context -- straxbra context
+    run_id  -- 5-digit zero padded run-id (str)
+
+    Arguments:
+    events     -- Provided by decorator (ndarray)
+
+    Keyword arguments:
+    x          -- Field name in events that is plotted on x-axis
+    y          -- Field name in events that is plotted on y-axis
+    cuts       -- Boolean array or list of indices to apply cuts to events
+                  if None nothing is applied (default None)
+    xbins      -- Number of bins or list of bin-edges (see np.histogram2d)
+                  If None default logspace is used (default None)
+    ybins      -- Number of bins or list of bin-edges (see np.histogram2d)
+                  If None default logspace is used (default None)
+    xlabel     -- Label of x-axis. If None use x (default None, str)
+    ylabel     -- Label of y-axis. If None use y (default None, str)
+    title      -- Title of Plot   (str)
+    cmap       -- Mpl-colormap used (default plasma)
+    xlog       -- Whether or not to plot x-axis logarithmicly (default True)
+    ylog       -- Whether or not to plot y-axis logarithmicly (default True)
+    figsize    -- mpl figsize (default (9, 5), tuple)
+    kwargs     -- Any kwargs plt.pcolormesh accepts
+    """
+
+    if cuts is not None:
+        if isinstance(cuts, int) or isinstance(cuts[0], int):
+            events = events.iloc[cuts]
+        else:
+            events = events.loc[cuts]
+
+    xbins = np.logspace(0, 6, 100) if xbins is None else xbins
+    ybins = np.logspace(0, 5, 100) if ybins is None else ybins
+
+    counts, _, _ = np.histogram2d(events[x], events[y], bins=[xbins, ybins])
+
+    plt.figure(figsize=figsize)
+    im = plt.pcolormesh(xbins, ybins, counts.T, cmap=cmap, norm=colors.LogNorm(), **kwargs)
+
+    plt.title(title)
+    plt.xlabel(xlabel if xlabel else x)
+    plt.ylabel(ylabel if ylabel else y)
+
+    if xlog: plt.xscale('log')
+    if ylog: plt.yscale('log')
+    plt.colorbar()
     plt.tight_layout()
+
+
